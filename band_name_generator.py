866dad8fb9d3d8c2ead200ba135a979ba4f2fcd9
akah-adakne <akashadakne2000@gmail.com>
2025-07-27 09:43:01 +0530

    Create band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,13 @@
+print("welcome to the bnd name generator ")
+a = input("what is the name of city you grew up in ")
+b = input("what is your pet name " )
+print("your band name should be " + a +" " + b )
+
+
+# "C:\Users\AKASH ADAKNE\PyCharmMiscProject\.venv\Scripts\python.exe" "C:\Users\AKASH ADAKNE\PyCharmMiscProject\band.py" 
+# welcome to the bnd name generator 
+# what is the name of city you grew up in london
+# what is your pet name jimmy
+# your band name should be london jimmy
+
+# Process finished with exit code 0
