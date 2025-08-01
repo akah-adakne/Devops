3c4c99793a319b0d6f2e1c9f6a5133dc0a60c3ec
akah-adakne <akashadakne2000@gmail.com>
2025-08-01 11:02:38 +0530

    Add files via upload

---
+++ Rollercoaster_eligibilty_criteria.py.py
@@ -0,0 +1,6 @@
+print("welcome to rollercoaster ride!")
+height= int(input("what is your height in cm? "))
+if height >= 120:
+    print("you are eligible for a rollercoaster ride")
+else:
+    print("sorry! you are not eligible for rollercoaster ride ")
