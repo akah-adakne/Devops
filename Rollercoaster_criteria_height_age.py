074d660e921c6700bbf127d11c7124dcd5121cb5
akah-adakne <akashadakne2000@gmail.com>
2025-08-01 11:54:48 +0530

    Update Rollercoaster_criteria_height_age.py

--- Rollercoaster_criteria_height_age.py
+++ Rollercoaster_criteria_height_age.py
@@ -11,3 +11,22 @@ if height >= 120:
         print("you should pay 20 rupess ")
 else:
     print("sorry! you are not eligible for rollercoaster ride ")
+
+# output 1 : height > 120 and age is >= 25
+# what is your height in cm? 130
+# you are eligible for a rollercoaster ride
+# my age is 25
+#  you should pay 100 rupees
+
+# Process finished with exit code 0
+
+
+# output 2 : height<120 
+# "C:\Users\AKASH ADAKNE\PyCharmMiscProject\.venv\Scripts\python.exe" "C:\Users\AKASH ADAKNE\PyCharmMiscProject\rollercoaster_height_age.py" 
+# welcome to rollercoaster ride!
+# what is your height in cm? 118
+# sorry! you are not eligible for rollercoaster ride 
+
+# Process finished with exit code 0
+
+
