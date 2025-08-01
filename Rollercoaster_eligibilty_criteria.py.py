c2c8eba3dcb6c8b05be09b0bee0fce174061aff0
akah-adakne <akashadakne2000@gmail.com>
2025-08-01 11:07:44 +0530

    Update Rollercoaster_eligibilty_criteria.py.py

--- Rollercoaster_eligibilty_criteria.py.py
+++ Rollercoaster_eligibilty_criteria.py.py
@@ -4,3 +4,20 @@ if height >= 120:
     print("you are eligible for a rollercoaster ride")
 else:
     print("sorry! you are not eligible for rollercoaster ride ")
+
+
+#eligible 
+# "C:\Users\AKASH ADAKNE\PyCharmMiscProject\.venv\Scripts\python.exe" "C:\Users\AKASH ADAKNE\PyCharmMiscProject\Rollercoaster_eligibilty_criteria.py.py" 
+# welcome to rollercoaster ride!
+# what is your height in cm? 122
+# you are eligible for a rollercoaster ride
+
+# Process finished with exit code 0
+
+# not eligible 
+# "C:\Users\AKASH ADAKNE\PyCharmMiscProject\.venv\Scripts\python.exe" "C:\Users\AKASH ADAKNE\PyCharmMiscProject\Rollercoaster_eligibilty_criteria.py.py" 
+# welcome to rollercoaster ride!
+# what is your height in cm? 90
+# sorry! you are not eligible for rollercoaster ride 
+
+# Process finished with exit code 0
